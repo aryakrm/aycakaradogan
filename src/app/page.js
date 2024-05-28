6831c749ce42c05cbab6d1f7a1a4591b0555e7ac
Arya Karimi <devaryakrm@gmail.com>
2024-05-28 22:57:15 +0300

    animations removed

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,3 @@
-import Animations from "./components/Animations/Animations";
 import Intro from "./components/Intro/Intro";
 import Sec2 from "./components/Sec2/Sec2";
 import Sec3 from "./components/Sec3/Sec3";
@@ -8,7 +7,6 @@ export default function Home() {
   return (
     <main>
       <Intro />
-      <Animations />
       <Welcome />
       <Sec2 />
       <Sec3 />
