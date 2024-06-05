462c47ad91e41e1291d5ff397f8a3892a3e630c0
Arya Karimi <devaryakrm@gmail.com>
2024-06-05 13:21:34 +0300

    designs added

--- src/app/components/Intro/Intro.jsx
+++ src/app/components/Intro/Intro.jsx
@@ -11,7 +11,7 @@ function Intro() {
       <Carousel fade>
         <Carousel.Item>
           <Image
-            src="https://i.ibb.co/Fw67Qqd/1.jpg"
+            src="https://i.ibb.co/sFj2xGg/1-min-1.jpg"
             alt="first Slide"
             width={100}
             height={100}
